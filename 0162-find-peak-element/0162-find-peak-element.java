f502d1bd9fa5eff34d9a652b0175a01c0d717589
Samadhan2005 <samjadhav8523@gmail.com>
2026-08-15 15:39:31 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.23%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,12 +5,12 @@ class Solution {
 
       while(left<right)
         {
-            int mid =left+(right-left)/2;
-            if(nums[mid]>nums[mid+1]){
-                right=mid;
+            int mid =left+(right-left+1)/2;
+            if(nums[mid]>nums[mid-1]){
+                left=mid;
             }
             else{
-                left=mid+1;
+                right=mid-1;
             }
         }
         return left;
