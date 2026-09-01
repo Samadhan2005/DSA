4144de4f230c460d64ef92374ab1ac6a1de4d0f9
Samadhan2005 <samjadhav8523@gmail.com>
2026-09-01 19:46:46 +0530

    Time: 0 ms (100%), Space: 42.2 MB (98.69%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,33 +1,35 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        // int count=0;
-        // int score=0;
+        int count=0;
+        int score=0;
 
-        // for(int i=0;i<s.length();i++){
-        //     if(s.charAt(i)=='('){
-        //        count++;
-        //     }
-        //     else{
-        //         count--;
-        //         if(s.charAt(i-1)=='('){
-        //             score+=1<<count;
-        //         }
-        //     }
-        // }
-        // return score;
-
-        Stack<Integer> stack=new Stack<>();
-        stack.push(0);
-        for(char ch:s.toCharArray()){
-            if(ch=='('){
-                stack.push(0);
-            }else{
-                int val=stack.pop();
-                int score=Math.max(2*val,1);
-                stack.push(score+stack.pop());
-
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+               count++;
+            }
+            else{
+                count--;
+                if(s.charAt(i-1)=='('){
+                    score+=1<<count;
+                }
             }
         }
-        return stack.pop();
+        return score;
+
+
+//using stack time and space complexity O(n)
+        // Stack<Integer> stack=new Stack<>();
+        // stack.push(0);
+        // for(char ch:s.toCharArray()){
+        //     if(ch=='('){
+        //         stack.push(0);
+        //     }else{
+        //         int val=stack.pop();
+        //         int score=Math.max(2*val,1);
+        //         stack.push(score+stack.pop());
+
+        //     }
+        // }
+        // return stack.pop();
     }
 }
