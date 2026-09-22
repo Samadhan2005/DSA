35fb4488e976f7bc5fb9b61968aa77a57165e378
Samadhan2005 <samjadhav8523@gmail.com>
2026-09-22 15:37:54 +0530

    Time: 1 ms (96.2%), Space: 47 MB (8.09%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,38 +15,84 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+
         List<List<Integer>> result=new ArrayList<>();
+
         if(root==null){
             return result;
         }
+
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
-        q.add(null);
-      ArrayList<Integer> level=new ArrayList<>();
-        while(!q.isEmpty()){
-            TreeNode currNode=q.remove();
-            if(currNode==null){
-                result.add(level);
-                if(q.isEmpty()){
-                    break;
-                }
-                else{
-                 level=new ArrayList<>();
-                    q.add(null);
-                }
-            }
-            else{
-                
-                level.add(currNode.val);
 
-                if(currNode.left!=null){
-                    q.add(currNode.left);
+        while(!q.isEmpty()){
+            int size=q.size();
+            ArrayList<Integer> level=new ArrayList<>();
+
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.remove();
+
+                level.add(curr.val);
+
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
             }
-              if(currNode.right!=null){
-                q.add(currNode.right);
-              }
-            }
+             
+        result.add(level);
+
         }
+
         return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     List<List<Integer>> result=new ArrayList<>();
+    //     if(root==null){
+    //         return result;
+    //     }
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.add(root);
+    //     q.add(null);
+    //   ArrayList<Integer> level=new ArrayList<>();
+    //     while(!q.isEmpty()){
+    //         TreeNode currNode=q.remove();
+    //         if(currNode==null){
+    //             result.add(level);
+    //             if(q.isEmpty()){
+    //                 break;
+    //             }
+    //             else{
+    //              level=new ArrayList<>();
+    //                 q.add(null);
+    //             }
+    //         }
+    //         else{
+                
+    //             level.add(currNode.val);
+
+    //             if(currNode.left!=null){
+    //                 q.add(currNode.left);
+    //         }
+    //           if(currNode.right!=null){
+    //             q.add(currNode.right);
+    //           }
+    //         }
+    //     }
+    //     return result;
     }
 }
